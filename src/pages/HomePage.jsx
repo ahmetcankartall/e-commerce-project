b5911dc121f1955b821d3a05464e897eff8cb1c0
ahmet can KARTAL <ahmetkrtl45@gmail.com>
2026-01-21 22:06:20 +0300

    Add Footer component to HomePage
    Imported and rendered the Footer component in HomePage to display it at the bottom of the page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import SectionOne from '../components/SectionOne';
 import SectionTwo from '../components/SectionTwo';
 import SectionThree from '../components/SectionThree';
-
+import Footer from '../layout/Footer';
 function HomePage() {
   return (
     <div className="snap-y snap-mandatory h-screen">
       <SectionOne />
       <SectionTwo />
       <SectionThree />
+      <Footer />
     </div>
   );
 }
