295e326ae02ec22273ae616534a35a8f3561e229
ahmetcankartall <ahmetkrtl45@gmail.com>
2026-02-03 20:37:02 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
+/*import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
 import { thunk } from 'redux-thunk'
 import logger from "redux-logger";
 
@@ -14,3 +14,34 @@ const rootReducer = combineReducers({
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk,logger))
+*/
+
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
+} from "redux";
+import { thunk } from "redux-thunk";
+import logger from "redux-logger";
+
+import clientReducer from "./reducers/clientReducer";
+import productReducer from "./reducers/ProductReducer";
+import shoppingCartReducer from "./reducers/shoppingCartReducer";
+
+const rootReducer = combineReducers({
+  client: clientReducer,
+  product: productReducer,
+  shop: shoppingCartReducer,
+});
+
+// 🔥 KRİTİK SATIR
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
+);
