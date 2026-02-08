325d9fc10b949306dfc38debc2a49ff6c5a3e984
ahmetcankartall <ahmetkrtl45@gmail.com>
2026-02-08 17:32:02 +0300

    Update CartDrawer.jsx

--- src/components/cart/CartDrawer.jsx
+++ src/components/cart/CartDrawer.jsx
@@ -32,8 +32,8 @@ export default function CartDrawer({ isOpen, onClose }) {
         `}
       >
         {/* Header */}
-        <div className="flex items-center justify-between px-4 py-4 border-b">
-          <h2 className="text-lg font-semibold">Sepetim</h2>
+        <div className="flex items-center justify-between px-4 py-4 border-b ">
+          <h2 className="text-lg font-semibold">Sepetim (Sepet Total)</h2>
           <button
             onClick={onClose}
             className="p-2 hover:bg-gray-100 rounded-lg"
@@ -44,10 +44,57 @@ export default function CartDrawer({ isOpen, onClose }) {
         </div>
 
         {/* Cart Items */}
-        <div className="flex-1 overflow-y-auto px-4 py-4 flex flex-col gap-4">
+        <div className="flex-1 overflow-y-auto px-4 py-4 flex flex-col gap-12 border-[#111111]">
           {/* Örnek item */}
-          <div className="flex items-center gap-4 border-b pb-4">
-            <div className="w-16 h-16 bg-gray-200 rounded-md" />
+          <div className="flex items-center gap-4 border-b pb-4  border-[#111111]">
+            <div className="w-24 h-40 bg-gray-200 rounded-md" />
+            <div className="flex flex-col  gap-3 ">
+              <span className="text-sm font-montserrat font-bold">Ürün Adı: (ürün adı)</span>
+              <span className="text-xs text-gray-500 font-montserrat">Beden: M</span>
+              <span className="text-sm font-semibold mt-1 ">Fiyat : ₺499</span>
+              <div className="  flex flex-row  bg-[#f5f5f5] border border-[#efefef]  p-1  self-start rounded-lg" >
+                <button className="px-2 py-1 text-sm  hover:cursor-pointer font-montserrat text-sm">-</button>
+                <span className="px-4 py-1 text-sm font-montserrat text-sm font-bold">1</span>
+                <button className="px-2 py-1 text-sm  hover:cursor-pointer rounded-lg font-montserrat text-sm">+</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4 border-b pb-4  border-[#111111]">
+            <div className="w-24 h-40 bg-gray-200 rounded-md" />
+            <div className="flex flex-col flex-1">
+              <span className="text-sm font-medium">Ürün Adı</span>
+              <span className="text-xs text-gray-500">Beden: M</span>
+              <span className="text-sm font-semibold mt-1">₺499</span>
+            </div>
+          </div>
+          
+          <div className="flex items-center gap-4 border-b pb-4  border-[#111111]">
+           <div className="w-24 h-32 bg-gray-200 rounded-md" />
+            <div className="flex flex-col flex-1">
+              <span className="text-sm font-medium">Ürün Adı</span>
+              <span className="text-xs text-gray-500">Beden: M</span>
+              <span className="text-sm font-semibold mt-1">₺499</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 border-b pb-4  border-[#111111]">
+            <div className="w-24 h-32 bg-gray-200 rounded-md" />
+            <div className="flex flex-col flex-1">
+              <span className="text-sm font-medium">Ürün Adı</span>
+              <span className="text-xs text-gray-500">Beden: M</span>
+              <span className="text-sm font-semibold mt-1">₺499</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 border-b pb-4  border-[#111111]">
+            <div className="w-24 h-32 bg-gray-200 rounded-md" />
+            <div className="flex flex-col flex-1">
+              <span className="text-sm font-medium">Ürün Adı</span>
+              <span className="text-xs text-gray-500">Beden: M</span>
+              <span className="text-sm font-semibold mt-1">₺499</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 border-b pb-4  border-[#111111]">
+           <div className="w-24 h-32 bg-gray-200 rounded-md" />
             <div className="flex flex-col flex-1">
               <span className="text-sm font-medium">Ürün Adı</span>
               <span className="text-xs text-gray-500">Beden: M</span>
@@ -60,15 +107,18 @@ export default function CartDrawer({ isOpen, onClose }) {
         </div>
 
         {/* Footer */}
-        <div className="border-t px-4 py-4 flex flex-col gap-3">
+        <div className="border-t px-4 py-4 flex flex-col gap-3 px-16">
           <div className="flex justify-between text-sm font-medium">
-            <span>Toplam</span>
+            <span>Genel Toplam</span>
             <span>₺499</span>
           </div>
-
-          <button className="w-full bg-[#23a6f0] text-white py-3 rounded-lg font-medium hover:bg-blue-600 transition-colors">
+ <button className="w-full bg-[#111111] text-white py-3 rounded-full font-medium hover:bg-white hover:text-[#111111] hover:cursor-pointer border transition-colors">
+            Sepeti Görüntüle
+          </button>
+          <button className="w-full bg-[#111111] text-white py-3 border rounded-full font-medium hover:cursor-pointer transition-colors">
             Satın Al
           </button>
+            
         </div>
       </div>
     </>
